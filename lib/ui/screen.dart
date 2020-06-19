5cae45421a81ff659df5c8d202ecdb646eaa3000
lifeofdan_el <anomfuemedaniel@gmail.com>
2020-06-19 03:09:57 +0100

    Added DropDownItemMenu for Network provider.

--- lib/ui/screen.dart
+++ lib/ui/screen.dart
@@ -1,13 +1,62 @@
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class LoginUI03 extends StatefulWidget {
+
   @override
   _LoginUI03State createState() => _LoginUI03State();
 }
 
+class Network {
+  int id;
+  String name;
+
+  Network(this.id, this.name);
+
+  static List<Network> getNetworks(){
+    return <Network> [
+      Network(1, 'MTN'),
+      Network(2, 'GLO'),
+      Network(3, 'AIRTEL'),
+      Network(4, '9MOBILE'),
+    ];
+  }
+}
+
+
+
 class _LoginUI03State extends State<LoginUI03> {
+
+  List<Network> _networks = Network.getNetworks();
+  List<DropdownMenuItem<Network>> _dropdownMenuItems;
+  Network _selectedNetwork;
+
+  @override
+  void initState() {
+    _dropdownMenuItems = buildDropDownMenuItems(_networks);
+    _selectedNetwork = _dropdownMenuItems[0].value;
+    super.initState();
+  }
+
+  List<DropdownMenuItem<Network>>  buildDropDownMenuItems(List networks){
+    List<DropdownMenuItem<Network>> items = List();
+    for (Network network in networks){
+      items.add(
+          DropdownMenuItem(
+            value: network,
+            child: Text(network.name),
+          ),
+      );
+    }
+    return items;
+  }
+  onChangeDropdownItem(Network selectedNetwork){
+    setState(() {
+      _selectedNetwork = selectedNetwork;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final double height = MediaQuery.of(context).size.height;
@@ -22,7 +71,7 @@ class _LoginUI03State extends State<LoginUI03> {
             child: Stack(
               children: <Widget>[
                 Positioned(
-                  top: height * 0.2,
+                  top: height * 0.1,
                   left: 50,
                   right:50,
                   child: FlutterLogo(
@@ -31,7 +80,7 @@ class _LoginUI03State extends State<LoginUI03> {
                   ),
                 ),
                 Positioned(
-                    top: height * 0.4,
+                    top: height * 0.3,
                     child: Container(
                       width: width,
                       child: Text(
@@ -46,7 +95,7 @@ class _LoginUI03State extends State<LoginUI03> {
                 ),
 
                 Positioned(
-                  top: height * 0.5,
+                  top: height * 0.4,
                   child: Container(
                     width: width * 0.8,
                     height: height * 0.4,
@@ -55,7 +104,8 @@ class _LoginUI03State extends State<LoginUI03> {
                       children: <Widget>[
                         TextField(
                           style: TextStyle(
-                              fontSize:16
+                              fontSize:16,
+                            color: Colors.white,
                           ),
                           cursorColor: Colors.white,
                           keyboardType: TextInputType.phone,
@@ -92,8 +142,10 @@ class _LoginUI03State extends State<LoginUI03> {
                         SizedBox(height: 20,),
                         TextField(
                           style: TextStyle(
-                              fontSize:16
+                              fontSize:16,
+                            color: Colors.white,
                           ),
+
                           keyboardType: TextInputType.phone,
                           cursorColor: Colors.white,
                           decoration: InputDecoration(
@@ -126,49 +178,25 @@ class _LoginUI03State extends State<LoginUI03> {
                             ),
                           ),
                         ),
-
-                        SizedBox(height: 20,),
-                        TextField(
+                        SizedBox(height: 10,),
+                        DropdownButton(
+                          style: TextStyle(color: Colors.white) ,
+                          value: _selectedNetwork,
+                          items: _dropdownMenuItems,
+                          onChanged: onChangeDropdownItem,
+                        ),
+                        SizedBox(height: 10,),
+                        Text('Network Provider is ${_selectedNetwork.name}',
                           style: TextStyle(
-                              fontSize:16
-                          ),
-                          keyboardType: TextInputType.phone,
-                          cursorColor: Colors.white,
-                          decoration: InputDecoration(
-                            fillColor: Colors.grey[700].withOpacity(0.5),
-                            filled: true,
-                            hintText: 'Network Provider',
-                            hintStyle: TextStyle(
-                              color: Colors.grey[100],
-                            ),
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(30),
-                                borderSide: BorderSide(
-                                    color:Colors.white,
-                                    width: 2
-                                )
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(30),
-                                borderSide: BorderSide(
-                                    color:Colors.grey,
-                                    width: 2
-                                )
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(30),
-                                borderSide: BorderSide(
-                                    color:Colors.white,
-                                    width: 2
-                                )
-                            ),
+                              color: Colors.white
                           ),
                         ),
-
                       ],
                     ),
                   ),
                 ),
+
+
 
 
 
